db89a253d725debe1a01e3e32ad8bb9afb912cd0
Bodor Tamas Levente <btamaslevente@gmail.com>
2022-03-10 12:04:13 +0100

    login submit func first introduced

--- client/client-app/src/Login.js
+++ client/client-app/src/Login.js
@@ -4,8 +4,30 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import {serveraddress} from './Server.js';
+
+function submit() {
+  const data = { username: 'example' };
+
+  fetch(serveraddress + '/login', {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
 
 function Login() {
+
   return (
     <>
     <Grid container spacing={2}>
@@ -13,11 +35,13 @@ function Login() {
       <Grid item xs={8} md={4}>
         <Card sx={{ mt : 8 , mx : "auto"}}>
           <CardContent>
-            <h2>Maintenance System</h2>
-            <h3>Please fill the fields to login.</h3>
+            <h1>Maintenance System</h1>
+            <h5>Please fill the fields to login.</h5>
+            <TextField id="username" label="Username" variant="outlined" /><br />
+            <TextField id="password" label="Password" type="password"/>
           </CardContent>
           <CardActions>
-            <Button size="medium">Log me in!</Button>
+            <Button size="large" variant="contained" color="success" onClick={submit}>Log me in!</Button>
           </CardActions>
         </Card>
       </Grid>
---
+++ client/client-app/src/Server.js
@@ -0,0 +1,1 @@
+export const serveraddress = 'localhost:8080';
