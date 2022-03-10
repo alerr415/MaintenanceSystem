51010cdaceee376d1638554239506d9cded4f1f7
Tálas Martin <70484866+MartinTalas@users.noreply.github.com>
2022-03-10 20:41:05 +0100

    Create Device.java

---
+++ server/src/main/java/com/mansys/server/backend/Device.java
@@ -0,0 +1,20 @@
+package com.mansys.server.backend;
+
+public class Device {
+
+    String category;
+    
+    public class Request {
+
+        public Request(){
+
+        }
+    }
+
+    public class Response {
+
+        public Response(){
+
+        }
+    }
+}
