7f3131bfc20090921e04c4267e3c635373a6c1c3
Toth-Balint1225 <74766154+Toth-Balint1225@users.noreply.github.com>
2022-03-10 21:16:46 +0100

    Basic REST controller functionallity for login.
    Uses the DatabaseManager and the Server class to check the database
    and create a response based on the database's result.

--- server/src/main/java/com/mansys/server/ServerApplication.java
+++ server/src/main/java/com/mansys/server/ServerApplication.java
@@ -1,10 +1,18 @@
 package com.mansys.server;
 
+import com.mansys.server.backend.Authenticate;
+import com.mansys.server.backend.Server;
 import com.mansys.server.data.DatabaseManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ServerApplication {
 
@@ -13,6 +21,23 @@ public class ServerApplication {
 		
 		DatabaseManager dbm = DatabaseManager.getInstance();
 		dbm.testProcedure();
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+
+	/**
+	 * Handle for an authentication request.
+	 * Tóth Bálint
+	 * @param request the POST HTTP-request's json body
+	 * @return a http response entity with the result json
+	 */
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> authenticate(@RequestBody Authenticate.Request request) {
+		Authenticate.Response response = Server.getInstance().handleAuthenticate(request);
+		return ResponseEntity.ok(response);
 	}
 
 }
--- server/src/main/java/com/mansys/server/backend/Server.java
+++ server/src/main/java/com/mansys/server/backend/Server.java
@@ -1,5 +1,7 @@
 package com.mansys.server.backend;
 
+import com.mansys.server.data.DatabaseManager;
+
 /**
  * The server class handles all the functionallity of the conceptual server.
  * It follows the singleton design pattern, as we want to have only one instance of the server to exists.
@@ -31,7 +33,31 @@ public class Server implements ServerInterface {
      * Authenticate handling.
      * implemented from ServerInterface
      */
+    @Override
     public Authenticate.Response handleAuthenticate(Authenticate.Request req){
-        throw new UnsupportedOperationException("This function is not implemented yet!");//dummy return
+        //throw new UnsupportedOperationException("This function is not implemented yet!");//dummy return
+		System.out.println("[SERVER]: Handle login request username: " + req.getUsername() + " password: " + req.getPassword());
+        // get the authentication data from the database
+        int res_code = DatabaseManager.getInstance().authenticateUser(req.getUsername(),req.getPassword());
+
+        // create and decode the return value into a response type
+        Authenticate.Response res = new Authenticate.Response();
+
+        // NOT THE FINAL 
+        switch (res_code) {
+            case 0: // good
+            {
+                res.setErrorCode(0);
+                res.setErrorMessage("Success");
+                res.setRole("@dummy_role@");
+            }
+            default:
+            {
+                res.setErrorCode(1);
+                res.setErrorMessage("Unknown Login");
+                res.setRole("@dummy_role@");
+            }
+        }
+        return res;
     }
 }
