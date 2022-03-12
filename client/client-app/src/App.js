4779bef73f016812eb8ab0fa267677de81a047bb
Bodor Tamas Levente <btamaslevente@gmail.com>
2022-03-12 21:18:55 +0100

    device manager actions added to layout

--- client/client-app/src/App.js
+++ client/client-app/src/App.js
@@ -4,7 +4,7 @@ import Layout from './Layout.js';
 function App() {
   return (
     <>
-      <Login />
+      <Layout />
     </>
   );
 
--- client/client-app/src/Layout.js
+++ client/client-app/src/Layout.js
@@ -1,19 +1,23 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
+//import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
+//import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
+//import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
+import ListItemButton from '@mui/material/ListItemButton';
+import AddIcon from '@mui/icons-material/Add';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import LogoutIcon from '@mui/icons-material/Logout';
 import Toolbar from '@mui/material/Toolbar';
+import MenuIcon from '@mui/icons-material/Menu';
 import Typography from '@mui/material/Typography';
 
 const drawerWidth = 240;
@@ -31,36 +35,47 @@ function ResponsiveDrawer(props) {
       <Toolbar />
 
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItemButton>
+          <ListItemIcon><AddIcon /></ListItemIcon>
+          <ListItemText>Eszköz hozzáadása</ListItemText>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon><FormatListBulletedIcon /></ListItemIcon>
+          <ListItemText>Eszközök megjelenítése</ListItemText>
+        </ListItemButton>
       </List>
 
       <Divider />
 
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItemButton>
+          <ListItemIcon><PlaylistAddIcon /></ListItemIcon>
+          <ListItemText>Kategória hozzáadása</ListItemText>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon><FormatListBulletedIcon /></ListItemIcon>
+          <ListItemText>Kategóriák megjelenítése</ListItemText>
+        </ListItemButton>
       </List>
+
+      <Divider />
+
+      <List>
+        <ListItemButton>
+          <ListItemIcon><LogoutIcon /></ListItemIcon>
+          <ListItemText>Kijelentkezés</ListItemText>
+        </ListItemButton>
+      </List>
+
+
     </div>
   );
 
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      //<CssBaseline />
+    <Box sx={{ display: 'flex' ,
+               backgroundColor: '#f3f1f1' }}>
 
       <AppBar
         position="fixed"
@@ -81,7 +96,7 @@ function ResponsiveDrawer(props) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            Maintenance System
+            Karbantartási rendszer
           </Typography>
         </Toolbar>
       </AppBar>
@@ -121,7 +136,9 @@ function ResponsiveDrawer(props) {
       </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{ flexGrow: 1,
+              p: 3,
+              width: { sm: `calc(100% - ${drawerWidth}px)` }}}
       >
         <Toolbar />
         <Typography paragraph>
@@ -155,13 +172,5 @@ function ResponsiveDrawer(props) {
     </Box>
   );
 }
-
-ResponsiveDrawer.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
 
 export default ResponsiveDrawer;
