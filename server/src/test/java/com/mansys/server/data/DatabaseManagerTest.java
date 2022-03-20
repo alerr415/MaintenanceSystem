8fa3a69dbbd365b9bf5bc597a25ed4e5d684508a
Toth-Balint1225 <74766154+Toth-Balint1225@users.noreply.github.com>
2022-03-20 13:47:53 +0100

    Added tests to the DatabaseManager

--- server/src/test/java/com/mansys/server/data/DatabaseManagerTest.java
+++ server/src/test/java/com/mansys/server/data/DatabaseManagerTest.java
@@ -1,20 +1,35 @@
 package com.mansys.server.data;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javafx.util.Pair;
 
 @SpringBootTest
 class DatabaseManagerTest {
 
 	@Test
 	void contextLoads() {
+        boolean fail = false;
+        try {
+            DatabaseManager dbm = DatabaseManager.getInstance(); 
+        } catch (Exception e) {
+            fail = true;
+        }
+        assertFalse(fail);
+
 	}
 
     @Test
-    void createSingletonInstance(){
-        DatabaseManager dbm = DatabaseManager.getInstance();
-        System.out.println(dbm.URL);
-        System.out.println(dbm.connectionIsActive);
+    void callLogin() {
+        Pair<Integer,String> ok = DatabaseManager.getInstance().authenticateUser("gonzalez","gonzalez321");
+        assertEquals(0,ok.getKey());
+        assertEquals("karbantarto", ok.getValue());
+        Pair<Integer,String> fail = DatabaseManager.getInstance().authenticateUser("not_me", "invalid69");
+        assertEquals(1, fail.getKey());
     }
 
 }
