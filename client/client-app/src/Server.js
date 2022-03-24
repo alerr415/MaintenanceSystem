0f4b7da8ac5f5dd7f476049b25f4e73e602fea4e
Bodor Tamas Levente <btamaslevente@gmail.com>
2022-03-24 17:54:55 +0100

    server address to local

--- client/client-app/src/Server.js
+++ client/client-app/src/Server.js
@@ -1,2 +1,2 @@
-//export const serveraddress = 'http://127.0.0.1:8080';
-export const serveraddress = 'https://579eca17-0e61-4371-966e-d21c6970dfd1.mock.pstmn.io';
+export const serveraddress = 'http://127.0.0.1:8080';
+//export const serveraddress = 'https://579eca17-0e61-4371-966e-d21c6970dfd1.mock.pstmn.io';
