cd0d71ba42b8966c5dc206d52f18a9846ea7def4
Toth-Balint1225 <74766154+Toth-Balint1225@users.noreply.github.com>
2022-04-08 14:15:27 +0200

    Added the Worker object for the resource

---
+++ server/src/main/java/com/mansys/server/backend/Worker.java
@@ -0,0 +1,81 @@
+package com.mansys.server.backend;
+
+public class Worker {
+    public static class Request {
+        private String lastName;
+        private String firstName;
+        private String qualification;
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getQualification() {
+            return qualification;
+        }
+
+        public void setLastName(String value) {
+            this.lastName=value;
+        }       
+        public void setFirstName(String value) {
+            this.firstName=value;
+        }
+        public void setQualification(String value) {
+            this.qualification=value;
+        }
+
+    }
+
+    public static class Response {
+        private int errorCode;
+        private String errorMessage;
+
+        public String getErrorMessage(){
+            return this.errorMessage;
+        }
+
+        public int getErrorCode(){
+            return this.errorCode;
+        }
+
+        public void setErrorMessage(String value){
+            this.errorMessage = value;
+        }
+
+        public void setErrorCode(int value){
+            this.errorCode = value;
+        }
+    }
+
+    public static class GetResponse {
+        private String lastName;
+        private String firstName;
+        private String qualification;
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getQualification() {
+            return qualification;
+        }
+
+        public void setLastName(String value) {
+            this.lastName=value;
+        }       
+        public void setFirstName(String value) {
+            this.firstName=value;
+        }
+        public void setQualification(String value) {
+            this.qualification=value;
+        }
+    }
+}
