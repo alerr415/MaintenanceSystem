739ef8da50ef92367f6c75652495ef05bf67a0d0
Toth-Balint1225 <74766154+Toth-Balint1225@users.noreply.github.com>
2022-04-22 16:11:33 +0200

    pojos implemented

---
+++ server/src/main/java/com/mansys/server/logic/Device.java
@@ -0,0 +1,7 @@
+package com.mansys.server.logic;
+
+public class Device {
+    public int id;
+    public String categoryName; // also id
+    
+}
---
+++ server/src/main/java/com/mansys/server/logic/TimerTask.java
@@ -0,0 +1,8 @@
+package com.mansys.server.logic;
+
+import java.sql.Date;
+
+public class TimerTask {
+    public String categoryName;  // also id
+    public Date referenceDate;
+}
